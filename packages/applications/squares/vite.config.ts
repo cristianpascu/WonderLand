6d12835f14429fd89b5c62410b06534c898b334c
Cristian Pascu <cristian.pascu@jugo.io>
2023-11-27 15:34:30 +0200

    Moved Squares app to its own package

--- packages/applications/squares/vite.config.ts
+++ packages/applications/squares/vite.config.ts
@@ -1,9 +1,9 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode, isPreview, isSsrBuild }) => {
-    console.log(command, mode, isPreview, isSsrBuild);
     return {
         plugins: [
             react({
